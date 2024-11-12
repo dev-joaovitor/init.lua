72d38dcb8bc9735b664a3b3f258443a85b282975
dev-joaovitor <joaotorvitor1@gmail.com>
2024-11-11 21:06:01 -0300

    some remappings

--- lua/john/remap.lua
+++ lua/john/remap.lua
@@ -1,2 +1,13 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- line move
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- navigating through
+-- search matches
+-- but the cursor
+-- stays on the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
