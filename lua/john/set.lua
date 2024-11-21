b4544e6890dfa75e3953bd207cfbbceb71e2240c
dev-joaovitor <joaotorvitor1@gmail.com>
2024-11-21 09:17:26 -0300

    set cindent and unset colorcolumn temporarily

--- lua/john/set.lua
+++ lua/john/set.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.cindent = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -18,7 +19,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
